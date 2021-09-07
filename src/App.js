bf3757afa35ac4ff3bfa1a1382cdcc1b0fe349e3
adnantaj1 <adnansulehri1@gmail.com>
2021-09-07 22:40:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import React, { useState } from 'react';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: "$23.4",
+      date: new Date(2021, 9, 10),
+    },
+    {
+      id: 2,
+      title: "New Desk",
+      amount: "$23.4",
+      date: new Date(2021, 9, 10),
+    },
+    {
+      id: 1,
+      title: "Health Care",
+      amount: "$23.4",
+      date: new Date(2021, 9, 10),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem title={expenses[1].title}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title}></ExpenseItem>
     </div>
   );
 }
